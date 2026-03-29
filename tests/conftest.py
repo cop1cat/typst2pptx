319a3b8b374eff0b4e1124ac8c8a5f0921a18878
cop1cat <notaskynet@yandex.ru>
2026-03-29 20:47:12 +0300

    applied formatter

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,4 @@ import pytest
 def minimal_pdf_bytes() -> bytes:
     doc = fitz.open()
     doc.new_page(width=595, height=842)
-    return doc.tobytes() # type: ignore[no-any-return]
+    return doc.tobytes()  # type: ignore[no-any-return]
